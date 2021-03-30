9ce0a89874a8d3eafd63a4ad0f740ffe45508e1e
TiagoPimentelNasajon <tiagopimentel@nasajon.com.br>
2021-03-30 14:46:57 -0300

    Criando template

--- fila_prioritaria.py
+++ fila_prioritaria.py
@@ -5,11 +5,6 @@ class FilaPrioritaria(FilaBase):
     def gera_senha_atual(self) -> None:
         self.senha_atual = f'PR{self.codigo}'
 
-    def atualiza_fila(self) -> None:
-        self.reseta_fila()
-        self.gera_senha_atual()
-        self.fila.append(self.senha_atual)
-
     def chama_cliente(self, caixa: int) -> str:
         cliente_atual: str = self.fila.pop(0)
         self.clientes_atendidos.append(cliente_atual)
@@ -23,6 +18,8 @@ class FilaPrioritaria(FilaBase):
             dados['dia'] = dia
             dados['agencia'] = agencia
             dados['clientes_atendidos'] = self.clientes_atendidos
-            dados['qnt_clientes_atendidos'] = len(self.clientes_atendidos)
+            dados['quantidade_clientes_atendidos'] = (
+                len(self.clientes_atendidos)
+            )
 
         return dados
